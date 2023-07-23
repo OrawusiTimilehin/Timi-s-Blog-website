d8be3b4f5466e74a7650bd451a4c5ddbfe582983
Oluwatimilehin Orawusi <timilehin.orawusi@gmail.com>
2023-07-23 21:01:08 +0100

    Created the structure for home page

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 from flask import Flask, render_template, url_for
 
 
-
-
-
-
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html", )
+    return render_template("index.html")
+
+@app.route("/blogs")
+def blogs():
+    return render_template("blogs.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 
 if __name__ == "__main__":
